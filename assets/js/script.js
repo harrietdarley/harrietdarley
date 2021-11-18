eb96e660246a3da31fa24fe0ad56242052b5512d
harrietdarley <harriet.darley@hotmail.co.uk>
2021-11-18 10:36:48 +0000

    new buttons, timer and working volume

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //Text Animation https://tobiasahlin.com
 // Get all the elements with the .ml2 class
-var textWrapper = document.querySelectorAll('.ml2');
+let textWrapper = document.querySelectorAll('.ml2');
 // Loop over the textWrapper array and apply the animation effect to each element in it
 textWrapper.forEach(el => {
     el.innerHTML = el.textContent.replace(
@@ -27,3 +27,48 @@ anime.timeline({ loop: true })
     });
 
 
+//VOLUME BUTTON ONE
+
+initAudioPlayer = () => {
+    const audio = new Audio();
+    audio.src = "assets/audio/bensound-allthat.mp3";
+    audio.loop = true;
+    audio.play();
+    //set object references
+    const volumebutton = document.getElementById("volume-icon");
+    //add event handling 
+    volumebutton.addEventListener("click", mute);
+    //functions
+    function mute() {
+        if (audio.muted) {
+            audio.muted = false;
+            audio.play();
+            volumebutton.src = "assets/images/icons8-volume-96.png";
+        } else {
+            audio.muted = true;
+            audio.pause();
+            volumebutton.src = "assets/images/icons8-mute-96.png";
+        }
+    }
+}
+window.addEventListener("load", initAudioPlayer);
+
+//VOLUME BUTTON 2 
+
+
+//HOME BUTTON 
+
+//TIMER
+let timeLeft = 30;
+const elem = document.getElementById('timerCountdown');
+const timerId = setInterval(countdown, 1000);
+
+function countdown() {
+    if (timeLeft == -1) {
+        clearTimeout(timerId);
+
+    } else {
+        elem.innerHTML = timeLeft + ':00';
+        timeLeft--;
+    }
+}
